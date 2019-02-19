624be24180f6371bfd1f74cd2251639644a816c5
Njoku Okechukwu Valentine <bonsoirval@gmail.com>
2019-02-19 16:31:01 +0100

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://anoruoshop.com/admin/');
+define('HTTP_CATALOG', 'http://anoruoshop.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://anoruoshop.com/admin/');
+define('HTTPS_CATALOG', 'http://anoruoshop.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/anoruoshop.com/html/admin/');
+define('DIR_SYSTEM', '/var/www/anoruoshop.com/html/system/');
+define('DIR_IMAGE', '/var/www/anoruoshop.com/html/image/');
+define('DIR_LANGUAGE', '/var/www/anoruoshop.com/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/anoruoshop.com/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/anoruoshop.com/html/system/config/');
+define('DIR_CACHE', '/var/www/anoruoshop.com/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/anoruoshop.com/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/anoruoshop.com/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/anoruoshop.com/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/anoruoshop.com/html/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/anoruoshop.com/html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'njoku');
+define('DB_PASSWORD', 'nuhTMX6z');
+define('DB_DATABASE', 'anoruoshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'an_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://anoruoshop.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://anoruoshop.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/anoruoshop.com/html/catalog/');
+define('DIR_SYSTEM', '/var/www/anoruoshop.com/html/system/');
+define('DIR_IMAGE', '/var/www/anoruoshop.com/html/image/');
+define('DIR_LANGUAGE', '/var/www/anoruoshop.com/html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/anoruoshop.com/html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/anoruoshop.com/html/system/config/');
+define('DIR_CACHE', '/var/www/anoruoshop.com/html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/anoruoshop.com/html/system/storage/download/');
+define('DIR_LOGS', '/var/www/anoruoshop.com/html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/anoruoshop.com/html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/anoruoshop.com/html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'njoku');
+define('DB_PASSWORD', 'nuhTMX6z');
+define('DB_DATABASE', 'anoruoshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'an_');
